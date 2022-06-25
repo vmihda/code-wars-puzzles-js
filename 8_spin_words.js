974365e87c7d984f6d8193053098f1504573bcc0
Vitalii MIhda <vitalik.migda@gmail.com>
2022-06-25 16:21:21 +0300

    Solving tasks with CodeWars

---
+++ 8_spin_words.js
@@ -0,0 +1,13 @@
+function spinWords(string){
+  const arr = string.split(' ');
+
+  return arr.map(item => {
+    if (item.length > 4){
+      return item.split('').reverse().join('')
+    }
+    return item
+  }).join(' ')
+}
+
+
+console.log("spinWords", spinWords('Just kidding there is still one more'))
