a2b1d003123e542db1e8a03516f355218c2b2c21
Vitalii MIhda <vitalik.migda@gmail.com>
2022-06-28 10:49:47 +0300

    Refactoring 8_spin_words.js

--- 8_spin_words.js
+++ 8_spin_words.js
@@ -1,15 +1,11 @@
 // Just kidding there is still one more -> Just gniddik ereht is llits one more
 
-function spinWords(string){
-  const arr = string.split(' ');
-
-  return arr.map(item => {
-    if (item.length > 4){
-      return item.split('').reverse().join('')
-    }
-    return item
-  }).join(' ')
-}
+const spinWords = (string) => string.split(' ').map(item => {
+	if (item.length > 4) {
+		return item.split('').reverse().join('')
+	}
+	return item
+}).join(' ')
 
 
 console.log("spinWords", spinWords('Just kidding there is still one more'))
